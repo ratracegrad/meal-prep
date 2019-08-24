590f50286404c77502942393f02d75d7415329df
Jennifer Bland <ratracegrad@gmail.com>
2019-08-24 10:47:02 -0400

    Updated drawer menu so it has links that work

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,11 +1,17 @@
 <template>
     <span>
-        <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="brown lighten-2"
+            dark
+            disable-resize-watcher
+        >
             <v-list>
                 <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
+                    <v-list-tile :key="index" :to="item.url">
                         <v-list-tile-content>
-                            {{item.title}}
+                            {{ item.title }}
                         </v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${index}`"></v-divider>
@@ -13,20 +19,39 @@
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <router-link to="/">
-                <v-toolbar-title data-cy="titleBtn">{{appTitle}}</v-toolbar-title>
+                <v-toolbar-title data-cy="titleBtn">{{
+                    appTitle
+                }}</v-toolbar-title>
             </router-link>
-            <v-btn flat class="hidden-sm-and-down nav-menu" to="/menu" data-cy="menuBtn">Menu</v-btn>
+            <v-btn
+                flat
+                class="hidden-sm-and-down nav-menu"
+                to="/menu"
+                data-cy="menuBtn"
+                >Menu</v-btn
+            >
             <v-spacer class="hidden-sm-and-down"></v-spacer>
             <div v-if="!isAuthenticated" class="hidden-sm-and-down">
                 <v-btn flat to="/sign-in" data-cy="signinBtn">SIGN IN</v-btn>
-                <v-btn color="brown lighten-3" to="/join" class="nav-join" data-cy="joinBtn">JOIN</v-btn>
+                <v-btn
+                    color="brown lighten-3"
+                    to="/join"
+                    class="nav-join"
+                    data-cy="joinBtn"
+                    >JOIN</v-btn
+                >
             </div>
             <div v-else>
                 <v-btn flat to="/about">PROFILE</v-btn>
-                <v-btn outline color="white" @click="logout" data-cy="logout">Logout</v-btn>
+                <v-btn outline color="white" @click="logout" data-cy="logout"
+                    >Logout</v-btn
+                >
             </div>
         </v-toolbar>
     </span>
@@ -40,10 +65,10 @@ export default {
             appTitle: 'Meal Prep',
             drawer: false,
             items: [
-                { title: 'Menu' },
-                { title: 'Profile' },
-                { title: 'Sign In' },
-                { title: 'Join' }
+                { title: 'Menu', url: '/menu' },
+                { title: 'Profile', url: '/about' },
+                { title: 'Sign In', url: '/sign-in' },
+                { title: 'Join', url: '/join' }
             ]
         };
     },
